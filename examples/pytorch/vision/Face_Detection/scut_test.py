7073a613faa19e1333126ce6f5751c58e7b4c011
Harsha Vardhan Simhadri <harsha-simhadri@users.noreply.github.com>
2020-12-25 13:23:28 -0800

    Update scut_test.py

--- examples/pytorch/vision/Face_Detection/scut_test.py
+++ examples/pytorch/vision/Face_Detection/scut_test.py
@@ -242,4 +242,3 @@ if __name__ == '__main__':
             score = dets[i][4]
             fout.write('{:.1f} {:.1f} {:.1f} {:.1f} {:.3f}\n'.
                        format(xmin, ymin, (xmax - xmin + 1), (ymax - ymin + 1), score))
-            
